1f1f49a511096f55389f6ec9eaee709840a3fe67
joser <joseronni12@gmail.co>
2024-06-25 23:01:59 -0600

    se argan cambios vistos en clase

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into curso(DESCRIPCION, NOMBRE)
+values ('Descripcion del objeto del noveno cutrimestre','Noveno');
+insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('Gestion de proyecto','1','3');
+insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('Desarrollo web','1','3');
+insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('Desarollo movil','1','3');
