6db809ff5d928a653b77ffe2691de0c33e8798f3
goosvin <leonkennedy498@gmail.com>
2024-06-29 16:01:42 -0600

    actividad 2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into curso (DESCRIPCION, NOMBRE) values ('nuevo curso','cursofinal');
+insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('matematicas','1','12');
+insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('programacion','1','2');
+insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('historia','1','5');
