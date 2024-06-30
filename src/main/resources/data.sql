5e9d358914eb5cede42e9d928e3b4581491eb144
larisotom@hotmail.com <larisotom@hotmail.com>
2024-06-29 18:25:25 -0600

    actividad2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into curso (DESCRIPCION, NOMBRE)
+    values ('Descripcion del objetivo del noveno cuatrimestre ', 'Noveno');
+
+    insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('Gestion de Proyectos', 1, 4);
+    insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('Desarrollo Web', 1, 5);
+    insert into materia (NOMBRE, ID_CURSO, HORAS_SEMANA) values ('Desarrollo Movil', 1, 6);
