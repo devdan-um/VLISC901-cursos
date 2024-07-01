6d0cd84b5261d7b266bf3da679aec87e788dfeb6
devdan <f.gomez@univermilenium.edu.mx>
2024-07-01 14:31:21 -0600

    cambios del entregable 2

---
+++ src/main/java/com/univer/cursos/model/request/CursoRequest.java
@@ -0,0 +1,24 @@
+package com.univer.cursos.model.request;
+
+import jakarta.persistence.Column;
+
+public class CursoRequest {
+    private String nombre;
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
