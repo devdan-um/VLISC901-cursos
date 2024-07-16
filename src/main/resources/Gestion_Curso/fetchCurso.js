f3662064ddf11e01ad6589019279138ae43a3d08
EmanuJT <emanuelmartinez2395@gmail.com>
2024-07-15 22:52:12 -0600

    Entregable parte dos producto final parte 2

---
+++ src/main/resources/Gestion_Curso/fetchCurso.js
@@ -0,0 +1,26 @@
+const curso ={
+    nombre: "Algebra",
+    descripcion: "Algebra Lineal"
+}
+
+fetch('http://localhost:8686/api/univer/materias/1',{
+    method: "GET",
+   // body: JSON.stringify(curso),
+    headers: {"content-type":"applicatio/json"}
+})
+
+.then(response =>{
+    console.log(response)
+    if(response.ok && response.status === 200){
+        console.log('dentro de la primera promesa')
+        return response.json()
+    }else{
+        throw Error (response.statusText)
+    }
+})
+.then(json=>{
+    console.log('segunda promesa')
+    console.log(json)
+    return json;
+})
+.catch(error=>console.log(error))
