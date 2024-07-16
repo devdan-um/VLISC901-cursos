55820392ba28f429bfe9f5f35099477b1094582e
joser <joseronni12@gmail.co>
2024-07-15 19:56:26 -0600

    ejercicio 2

---
+++ src/main/resources/entregable/fetchStudent.js
@@ -0,0 +1,29 @@
+const curso = {
+    name  : "cuatrimestre 1",
+    descripcion : "estudiante"
+
+}
+
+console.log(curso)
+console.log(JSON.stringify (curso))
+
+fetch('http://localhost:8686/api/univer/materias/save',{
+    method : "POST",
+    body : JSON.stringify (curso),
+    headers : {"content-type" : "application/json"}
+})
+.then( response => {
+    console.log(response)
+    if(response.ok && response.status === 201){
+        console.log('dentro de la primer promesa')
+        return response.json()
+    }else{
+        throw Error(response.statusText) 
+    }
+})
+.then(json =>{
+    console.log('segunda promesa')
+    console.log(json)
+    return json;
+})
+.catch(error => console.log (error))
