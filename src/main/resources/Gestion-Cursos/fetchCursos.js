a16141c0c0b3ef046c1aca0b8ab30b597e08fd33
Frn07 <francisco037.flores@gmail.com>
2024-07-22 11:31:38 -0600

    Entregable 16/07/2024

---
+++ src/main/resources/Gestion-Cursos/fetchCursos.js
@@ -0,0 +1,23 @@
+  const curso = {
+        nombre: "Matematicas",
+        descripcion: "Matematicas discretas"
+    }
+    fetch('http://localhost:8686/api/univer/materias/1', {
+        method: "GET",
+        //body: JSON.stringify(curso),
+        headers: {"Content-Type": "application/json"}
+    })
+    .then(response => {
+        if(response.ok && response.status===200){
+            console.log("Dentro de la primera promesa")
+            return response.json()
+        }else{
+            throw Error(response.statusText)
+        }
+    })
+    .then(json =>{
+        console.log("Segunda promesa")
+        console.log(json);
+        return json;
+    })
+    .catch(error => {console.log(error)})
